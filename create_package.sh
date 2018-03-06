87e23978a7bccd64a86346c5022be599b22c79b8
Nikita Zayakin <nzayakin@phoenixit.ru>
2018-03-06 14:12:14 +0300

    add config forming for single environment on single app server

--- create_package.sh
+++ create_package.sh
@@ -18,7 +18,9 @@ package_name=$(printf "%s_v%s" "$cid_and_pi" "$ver")
 pwd=$(pwd)
 package_dir=$pwd/$package_name
 hybris_packages_dir=$hybris_dir/hybris/temp/hybris/hybrisServer
-
+dev_config_dir=$hybris_dir/hybris/config
+package_config_dir=$package_dir/hybris/config
+package_bin_dir=$package_dir/hybris/bin
 #remove 'cp' to 'cp -i' alias if any
 #unalias cp 2>/dev/null
 
@@ -26,8 +28,7 @@ hybris_packages_dir=$hybris_dir/hybris/temp/hybris/hybrisServer
 #echo basefiles dir $basefiles_dir
 #echo curdir+pack $pwd/$package_name
 #echo package dir $package_dir
-#echo hybris_packages_dir $hybris_packages_dir
-time 
+echo hybris_packages_dir $hybris_packages_dir
 
 #remove folder if exists
 if [ -d "$package_dir" ]; then
@@ -37,16 +38,17 @@ fi
 
 if [ -f "$package_dir.zip" ]; then
   echo package zip already exists, renamed it
+  #append file modification datetime to package zip file
   file_date=$(stat -c%y $package_dir.zip | cut -d'.' -f1 | tr ' ' '_')
-  mv $package_dir.zip $package_dir$file_date.zip
+  mv "$package_dir.zip" "$package_dir$file_date.zip"
 fi
 
 echo create directory structure
-if [ ! -d "$package_dir/hybris/bin" ]; then
-  mkdir -p "$package_dir/hybris/bin"
+if [ ! -d "$package_bin_dir" ]; then
+  mkdir -p "$package_bin_dir"
 fi
-if [ ! -d "$package_dir/hybris/config" ]; then
-  mkdir -p "$package_dir/hybris/config"
+if [ ! -d "$package_config_dir" ]; then
+  mkdir -p "$package_config_dir"
 fi
 
 #build packages
@@ -70,8 +72,10 @@ if [ ${#environments[@]} -gt 1 ]; then
   exit $ERRCODE;
 else
   echo creating config for one environment;
-  
+  cp $dev_config_dir/local.properties $package_config_dir/customer.properties
+  cp $dev_config_dir/localextensions.xml $package_config_dir/localextensions.xml
 fi
+  echo config created
 
 echo creating archive
 zip -r $package_name.zip $package_name
