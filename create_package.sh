6c259213d4ca6e311fb341ef95250ef897fe7e09
Nikita Zayakin <nzayakin@phoenixit.ru>
2018-03-07 12:30:45 +0300

    add quotes at all echo commands

--- create_package.sh
+++ create_package.sh
@@ -20,6 +20,8 @@ ver=0.0.1
 hybris_dir=/opt/hybris6_6
 #array or environments values, can be: (dev,qa,stage,prod)
 declare -a environments=(dev)
+#datahub_webapp_full_filename
+datahub_webapp_filename=datahub-webapp-6.6.0.1-RC2.war
 
 #generated vars
 script_dir=/opt/dep_packages
@@ -28,75 +30,90 @@ package_name=$(printf "%s_v%s" "$cid_and_pi" "$ver")
 pwd=$(pwd)
 package_dir=$pwd/$package_name
 hybris_packages_dir=$hybris_dir/hybris/temp/hybris/hybrisServer
+datahub_webapp_dir=$hybris_dir/hybris/bin/ext-integration/datahub/web-app
+datagub_webapp_file=$datahub_webapp_dir/$datahub_webapp_filename
 dev_config_dir=$hybris_dir/hybris/config
 package_config_dir=$package_dir/hybris/config
 package_bin_dir=$package_dir/hybris/bin
+package_datahub_dir=$package_dir/datahub
+package_datahub_bin_dir=$package_datahub_dir/bin
+package_datahub_bin_file=$package_datahub_bin_dir/datahub-webapp.war
+package_datahub_config_dir=$package_datahub_dir/config
 #remove 'cp' to 'cp -i' alias if any
-#unalias cp 2>/dev/null
+#unalias cp #2>/dev/null
 
-echo script starts
-#echo basefiles dir $basefiles_dir
-#echo curdir+pack $pwd/$package_name
-#echo package dir $package_dir
-#echo hybris_packages_dir $hybris_packages_dir
+echo "script starts"
+echo "basefiles dir $basefiles_dir"
+echo "curdir+pack $pwd/$package_name"
+echo "package dir $package_dir"
+echo "hybris_packages_dir $hybris_packages_dir"
 
 #remove folder if exists
 if [ -d "$package_dir" ]; then
-  echo package folder already exists, removed old package folder
+  echo "package folder already exists, removed old package folder"
   rm -rf $package_dir
 fi
 
 if [ -f "$package_dir.zip" ]; then
-  echo package zip already exists, renamed it
+  echo "package zip already exists, renamed it"
   #append file modification datetime to package zip file
   file_date=$(stat -c%y $package_dir.zip | cut -d'.' -f1 | tr ' ' '_')
   mv "$package_dir.zip" "$package_dir$file_date.zip"
 fi
 
-echo create directory structure
+echo "create directory structure"
 if [ ! -d "$package_bin_dir" ]; then
   mkdir -p "$package_bin_dir"
 fi
 if [ ! -d "$package_config_dir" ]; then
   mkdir -p "$package_config_dir"
 fi
+if [ ! -d "$package_datahub_bin_dir" ]; then
+  mkdir -p "$package_datahub_bin_dir"
+fi
+if [ ! -d "$package_datahub_config_dir" ]; then
+  mkdir -p "$package_datahub_config_dir"
+fi
+
 
 #build packages
 if [ "$BUILD" = true ] ; then
-  echo building packages
+  echo "building packages"
   cd $hybris_dir/hybris/bin/platform
   ant production #2>/dev/null
   cd $script_dir
 else
-  echo omitting package creatiion
+  echo "omitting ant production command"
 fi
 
-echo create metadata.properties
+echo "create metadata.properties"
 cp $basefiles_dir/metadata.properties $package_dir
-echo copy packages into /hybris/bin
+echo "copy packages into /hybris/bin"
 yes | cp -rf $hybris_packages_dir/hybrisServer-Platform.zip $package_dir/hybris/bin
 yes | cp -rf $hybris_packages_dir/hybrisServer-AllExtensions.zip $package_dir/hybris/bin
+echo "copy data hub file"
+yes | cp -rf $datagub_webapp_file $package_datahub_bin_file
 
-echo create config
-echo ${#environments[@]} is number of environments
+echo "create config"
+echo "${#environments[@]} is number of environments"
 
 if [ ${#environments[@]} -gt 1 ]; then
   # create config for every environment
-  echo creating config for more than one environment isn\'t ready yet;
+  echo "creating config for more than one environment isn\'t ready yet"
   exit $ERRCODE;
 else
-  echo creating config for one environment;
+  echo "creating config for one environment"
   cp $dev_config_dir/local.properties $package_config_dir/customer.properties
   cp $dev_config_dir/localextensions.xml $package_config_dir/localextensions.xml
 fi
-  echo config created
+  echo "config created"
 
-echo creating archive
+echo "creating archive"
 zip -r $package_name.zip $package_name
 
-echo package created
-echo create MD5 file
+echo "package created"
+echo "create MD5 file"
 md5sum $package_name.zip > $package_name.md5
-echo check MD5 file
+echo "check MD5 file"
 md5sum -c $package_name.md5
 
